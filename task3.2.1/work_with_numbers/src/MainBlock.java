41560318f58ff928c92c345f8ee26435990a89e5
Sedach <veronikasedac57@gmail.com>
2018-10-11 18:32:34 +0300

    task3.2.1 update
    delete class SearchEngine, and some refactoring in MainBlock

--- task3.2.1/work_with_numbers/src/MainBlock.java
+++ task3.2.1/work_with_numbers/src/MainBlock.java
@@ -1,16 +1,16 @@
 import by.kvrnk.modules.Converter;
 import by.kvrnk.modules.Randomization;
-import by.kvrnk.modules.SearchEngine;
+
+import java.util.Collections;
 
 public class MainBlock {
 
     public static void main(String[] args) {
         Randomization randomization = new Randomization();
         Converter converter = new Converter();
-        SearchEngine searchEngine = new SearchEngine();
 
         int number = randomization.generateThreeDigitNumber();
-        int max = searchEngine.maxNumber(converter.toArrayList((Integer.toString(number).toCharArray())));
+        int max = Collections.max(converter.toArrayList((Integer.toString(number).toCharArray())));
 
         System.out.println(number + "; " + max);
 
